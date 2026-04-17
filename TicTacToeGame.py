691a3b1c16eb750edf4efc74ab76514464f4d298
Robert Palmer <r91palmer@gmail.com>
2026-04-17 16:32:01 -0500

    Added in vars for messages, players and potential board markings

--- TicTacToeGame.py
+++ TicTacToeGame.py
@@ -1,3 +1,18 @@
+welcome_message = f'Welcome to Tic-Tac-Toe!'
+game_ending_message = f'Thanks for playing!'
+players = ['X','O']
+boardmarks = { 
+    1: '',
+    2: '',
+    3: '',
+    4: '',
+    5: '',
+    6: '',
+    7: '',
+    8: '',
+    9: ''
+ }
 
 
-print('Hello World')
+print(boardmarks)
+print(welcome_message)
