9bb9ca3d8bbe81285b81283e33304a68171ad4d0
Robert Palmer <r91palmer@gmail.com>
2026-04-20 20:25:39 -0500

    Worked on initial configuring of the game board

--- TicTacToeGame.py
+++ TicTacToeGame.py
@@ -2,17 +2,29 @@ welcome_message = f'Welcome to Tic-Tac-Toe!'
 game_ending_message = f'Thanks for playing!'
 players = ['X','O']
 boardmarks = { 
-    1: '',
-    2: '',
-    3: '',
-    4: '',
-    5: '',
-    6: '',
-    7: '',
-    8: '',
-    9: ''
+    1: ' ',
+    2: ' ',
+    3: ' ',
+    4: ' ',
+    5: ' ',
+    6: ' ',
+    7: ' ',
+    8: ' ',
+    9: ' '
  }
+board_keys = [k for k in boardmarks]
 
+board = []
 
-print(boardmarks)
-print(welcome_message)
+def display_board():
+    for key in boardmarks:
+        if key in [3,6,9]:
+            board.append(f'{boardmarks[key]}\n-+-+-\n')
+        else:
+            board.append(f'{boardmarks[key]}|')
+    return board
+
+print(''.join(display_board()))
+##print(boardmarks)
+##print(welcome_message)
+##print(board_keys)
