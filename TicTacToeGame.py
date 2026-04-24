d3843fcf8d6d4e2c9a4e71f43c732ded4d0375b2
Robert Palmer <r91palmer@gmail.com>
2026-04-24 16:26:42 -0500

    working out logic to determine if player moves match any winning combinations

--- TicTacToeGame.py
+++ TicTacToeGame.py
@@ -15,6 +15,8 @@ boardmarks = {
     9: ' '
  }
 board_keys = [k for k in boardmarks]
+
+## Conditionals to determine the state of the game
 winning_combinations = [
     [1,2,3],
     [4,5,6],
@@ -25,12 +27,20 @@ winning_combinations = [
     [3,6,9]
 ]
 
+move_log = {
+    'X': [1,2,3],
+    'O': []
+}
+
+game_over_flag = False
+
 ## When new game starts, randomly pick which player gets to go first
 current_player = players[random.randrange(0,2)]
 
 ## Update Board with the selection that the current player has made
 player_move = int(input(f"What is {current_player}'s move? (1-9))"))
 boardmarks[player_move] = current_player
+move_log[current_player].append(player_move)
 
 ## Once when mark has been stored, set the current player
 current_player = [player for player in players if player != current_player]
@@ -47,5 +57,19 @@ def display_board():
             board.append(f'{boardmarks[key]}|')
     return board
 
-print(''.join(display_board()), board_keys)
-print(current_player)
+## Logic to persist game until either player reaches a winning combination/game is a tie
+## while game_over_flag == False:
+##    print('Test!!!')
+##    game_over_flag = True
+
+for key in move_log:
+    for combination in winning_combinations:
+        print(move_log[key], combination)
+        if combination == move_log[key]:
+            print('Winning combination!')
+        else:
+            print('Not a winning combination!')
+
+## print(''.join(display_board()))
+## print(current_player)
+## print(move_log)
