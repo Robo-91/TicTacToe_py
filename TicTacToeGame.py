5fa9f0cfa0a2b9a0538b331c2a626b88a02c0a93
Robert Palmer <r91palmer@gmail.com>
2026-05-14 16:04:07 -0500

    added logic to evaluate if player has made a winning combination, added logic for persisting the game, and cleaned up code by removing print statements

--- TicTacToeGame.py
+++ TicTacToeGame.py
@@ -36,19 +36,15 @@ game_over_flag = False
 
 ## When new game starts, randomly pick which player gets to go first
 current_player = players[random.randrange(0,2)]
-
-## Update Board with the selection that the current player has made
-player_move = int(input(f"What is {current_player}'s move? (1-9))"))
-boardmarks[player_move] = current_player
+current_player_string = "".join(current_player)
 
 ## Keep a log of all moves that each player has made
-for moves, winning_combo in zip(move_log[current_player],winning_combinations):
-    if player_move in winning_combo:
-        moves.append(player_move)
-
-## Once when mark has been stored, set the current player
-current_player = [player for player in players if player != current_player]
-
+def log_player_moves(log, winning_combinations):
+    for moves, winning_combo in zip(log, winning_combinations):
+        if player_move in winning_combo:
+            moves.append(player_move)
+        moves.sort()
+    
 board = []
 
 ## Display Board with selections that players have made
@@ -61,20 +57,29 @@ def display_board():
             board.append(f'{boardmarks[key]}|')
     return board
 
+## Determine if any possible moves made by the player matches with any of the winning combinations
+def determine_winner(current_player_moves, winning_combinations):
+    for value, winning_combo in zip(current_player_moves,winning_combinations):
+        if value == winning_combo:
+            return True
+        else:
+            return False
+
 ## Logic to persist game until either player reaches a winning combination/game is a tie
-## while game_over_flag == False:
-##    print('Test!!!')
-##    game_over_flag = True
+while game_over_flag == False:
+    ## Update Board with the selection that the current player has made
+    player_move = int(input(f"What is {current_player_string}'s move? (1-9))"))
+    boardmarks[player_move] = current_player_string
 
-for key in move_log:
-    for combination in winning_combinations:
-        print(combination, key)
-print(move_log)
+    ## Display Board
+    print(''.join(display_board()))
+    
+    ## Log the move the current player has made
+    log_player_moves(move_log[current_player_string], winning_combinations)
 
-## print(''.join(display_board()))
-## print(current_player)
-## print(move_log)
-##mylist = [3,1,2]
-##mylist.sort()
-##print(mylist == [1,2,3])
-print(current_player)
+    ## Determine if the player has made a winning combination
+    game_over_flag = determine_winner(move_log[current_player_string], winning_combinations)
+
+    ## Once when mark has been stored, set the current player
+    current_player = [player for player in players if player != current_player_string]
+    current_player_string = "".join(current_player)
